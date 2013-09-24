a5abd6fa0ffe288926e2a4e0a4e48d5e38d479d7
Zee Spencer <zee@zeespencer.com>
2013-09-24 15:37:50 -0700

    User must click respond before they can response

---
+++ public/js/showResponseForm.js
@@ -0,0 +1,11 @@
+function showResponseForm(questionElements) {
+  console.log(questionElements);
+  for(var qi = 0; qi < questionElements.length; qi++) {
+    var question = questionElements[qi];
+    var responseForm = questionElements[qi].querySelector('.response_form')
+    responseForm.classList.add('hidden');
+    question.querySelector('.show_response_form').addEventListener('click', function(e) {
+      responseForm.classList.remove('hidden');
+    });
+  }
+}
